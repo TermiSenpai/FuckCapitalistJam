85283ebe9b9cac0e92e29764f5d6dcfb0af5cee1
TermiSenpai <escarpaprieto@gmail.com>
2023-05-31 10:55:35 +0200

    object interactuable

--- Assets/Scripts/Items/ItemObject.cs
+++ Assets/Scripts/Items/ItemObject.cs
@@ -2,7 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemObject : MonoBehaviour
+public class ItemObject : MonoBehaviour, IInteractuable
 {
     [SerializeField] private ItemData item;
+
+    public string GetInteractPromt()
+    {
+        return string.Format($"Interact {item.displayName}");
+    }
+
+    public void OnInteract()
+    {
+        //Temporal
+        Destroy(gameObject);
+    }
 }
