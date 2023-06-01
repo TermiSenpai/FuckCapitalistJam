5633af399a0e737a19346301ba6fd40cc223802d
TermiSenpai <escarpaprieto@gmail.com>
2023-06-01 10:50:28 +0200

    Animator controller

--- Assets/Scripts/Player/AnimController/PlayerAnimController.cs
+++ Assets/Scripts/Player/AnimController/PlayerAnimController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerAnimController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Animator animator;
+
+    public string ATTACK1 = "Attack_1";
+    public string ATTACK2 = "Attack_2";
+
+    string currentAnimationState;
+
+    public void ChangeAnimationState(string newState)
     {
-        
+        // STOP THE SAME ANIMATION FROM INTERRUPTING WITH ITSELF //
+        if (currentAnimationState == newState) return;
+
+        // PLAY THE ANIMATION //
+        currentAnimationState = newState;
+        animator.CrossFadeInFixedTime(currentAnimationState, 0.2f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
