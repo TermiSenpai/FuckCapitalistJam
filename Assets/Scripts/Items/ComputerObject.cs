defe2e9ef71a9792b4e1f5d6dd0c26dbf1780029
TermiSenpai <escarpaprieto@gmail.com>
2023-06-05 09:18:06 +0200

    unnecesary code deleted

--- Assets/Scripts/Items/ComputerObject.cs
+++ Assets/Scripts/Items/ComputerObject.cs
@@ -8,13 +8,11 @@ public class ComputerObject : ItemObject
     [SerializeField] CinemachineVirtualCamera computerCamera;
     [SerializeField] CameraManager camManager;
     [SerializeField] PlayerManager playerManager;
-    [SerializeField] ComputerManager computerManager;
 
     public override void OnInteract()
     {
         camManager.switchCam(computerCamera);
         playerManager.EnablePlayerInputs(false);
         playerManager.changeCursorState(CursorLockMode.None);
-        //isInteractuable = false;
     }
 }
