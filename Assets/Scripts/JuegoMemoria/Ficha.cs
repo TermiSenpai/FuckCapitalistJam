6a7fddad132386809b2ad8d057a2864d8c2abca6
Florin <fkorincat1@gmail.com>
2023-06-09 11:59:55 +0200

    refresh, suffle & resolved animation

--- Assets/Scripts/JuegoMemoria/Ficha.cs
+++ Assets/Scripts/JuegoMemoria/Ficha.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.EventSystems;
 
 public class Ficha : MonoBehaviour
 {
     [SerializeField] private Animator m_Animator;
+    protected string m_Name;
+    [SerializeField] private TextMeshProUGUI m_text;
+    [SerializeField] private Image cara;
+    private int veces = 0;
 
 
     // Start is called before the first frame update
@@ -29,16 +35,33 @@ public class Ficha : MonoBehaviour
         this.transform.localScale = new Vector3(0.5f, 0.1f, 0.5f);
         
     }*/
+    public void changeName(string name) { 
+        m_Name = name;
+        m_text.text = name; 
+    }
 
 
     public void MostrarFrente()
     {
         Debug.Log("clicked");
         m_Animator.Play("FichaReversoAlFrente");
+        Invoke("changeColor", 0.5f);
+    }
+
+    private void changeColor()
+    {
+        if(veces%2==0)
+        cara.color = new Color32(255, 255, 225, 100);
+        else
+        cara.color = new Color32(255, 0, 0, 100);
+
+        veces++;
     }
     public void MostrarReverso()
     {
         Debug.Log("clicked");
         m_Animator.Play("FichaFrenteAlReverso");
+        Invoke("changeColor", 0.5f);
     }
+
 }
--- Assets/Scripts/JuegoMemoria/JuegoMemoria.cs
+++ Assets/Scripts/JuegoMemoria/JuegoMemoria.cs
@@ -1,47 +1,110 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class JuegoMemoria : MonoBehaviour
 {
     [Header("Valores")]
     //public GameObject plano;
-    [SerializeField] float m_PlayAreaX = 2;
-    [SerializeField] float m_PlayAreaY = 2;
+    [SerializeField] int m_PlayAreaX = 2;
+    [SerializeField] int m_PlayAreaY = 2;
+    [SerializeField] int m_NumeroPares = 0;
     /*[SerializeField]*/ Vector2 m_SpaceBetweenChips = new Vector2(64,88);
-    [SerializeField] Vector2 m_MaxSize = Vector2.zero;
+    /*[SerializeField]*/ Vector2 m_MaxSize /*= Vector2.zero*/;
 
     [Header("Referencias")]
     [SerializeField] GameObject m_Chip;
     [SerializeField] RectTransform m_PlayArea;
     [SerializeField] TextMeshProUGUI Timer;
+    [SerializeField] Button ResfreshButton;
+
+
     private float m_Time = 3;
 
-    private bool m_IsPlaying = true;
+    private List<GameObject> Cards;
+    public List<int> CardsIds;
+
+    public bool m_IsPlaying = true;
 
     void Start()
     {
+        Cards = new List<GameObject>();
+        createCards();
+        ResfreshButton.onClick.AddListener(delegate { RefreshCards(); });
+    }
+
+
+    //crear Cartas
+    /// <summary>
+    /// createCards
+    /// MezclaIds
+    /// RemoveCards
+    /// </summary>
+    private void createCards()
+    {
+        m_NumeroPares = m_PlayAreaX * m_PlayAreaY;
+        MezclaIds();
+        //CardsIds = MezclaIds();
         m_MaxSize = new Vector2(m_PlayArea.rect.width, m_PlayArea.rect.height);
         //plano.transform.localScale = new Vector3(m_MaxSize.x,0.1f, m_MaxSize.y);
         Debug.Log(m_Chip.transform.localScale);
         Debug.Log(m_MaxSize);
+
         //El Area de juego esta en la mitad de la pantalla, calculamos el tamaño del tablero para que se encuentre en el medio
-        
+
         Vector2 chipsize = ChipSize();
         m_SpaceBetweenChips = new Vector2(m_SpaceBetweenChips.x * chipsize.x, m_SpaceBetweenChips.y * chipsize.y);
         Vector2 posicionInicialFicha = CalcularPosicionFicha();
         //creamos la fichas
-        for (int x=0; x < m_PlayAreaX; x++)
+        for (int x = 0; x < m_PlayAreaX; x++)
         {
-            for(int y=0; y< m_PlayAreaY; y++)
+            for (int y = 0; y < m_PlayAreaY; y++)
             {
-                GameObject fichaGO = Instantiate(m_Chip, new Vector3(0,0,0),Quaternion.identity);
+                GameObject fichaGO = Instantiate(m_Chip, new Vector3(0, 0, 0), Quaternion.identity);
+                fichaGO.GetComponent<Ficha>().changeName(CardsIds[(m_PlayAreaY * x) + y].ToString());
+                Cards.Add(fichaGO);
                 fichaGO.transform.localScale = new Vector3(chipsize.x, chipsize.x, 0.1f);
 
                 fichaGO.transform.SetParent(m_PlayArea);
-                fichaGO.transform.localPosition = new Vector3((x * m_SpaceBetweenChips.x)-posicionInicialFicha.x,  (y * m_SpaceBetweenChips.y)-posicionInicialFicha.y,0);
+                fichaGO.transform.localPosition = new Vector3((x * m_SpaceBetweenChips.x) - posicionInicialFicha.x, (y * m_SpaceBetweenChips.y) - posicionInicialFicha.y, 0);
             }
+        }
+    }
+
+    private void MezclaIds()
+    {
+        List<int> idsCards = new List<int>();
+        {
+            for(int i = 0;i < m_NumeroPares; i++)
+            {
+                idsCards.Add(i/2);
+            }
+
+            CardsIds = idsCards;
+            Shuffle(CardsIds);
+            //CardsIds
+        }
+    }
+
+    private void RefreshCards()
+    {
+        RemoveCards();
+        createCards();
+    }
+
+    private void RemoveCards()
+    {
+        foreach (GameObject go in Cards)
+        {
+            Destroy(go);
+        }
+        Debug.Log(Cards.Count);
+        for (var i = (Cards.Count-1); i >= 0; i--)
+        {
+            Cards.RemoveAt(i);
         }
     }
 
@@ -51,6 +114,14 @@ public class JuegoMemoria : MonoBehaviour
         if(m_IsPlaying)
         CountDown();    
     }
+
+
+
+    //POSICION
+    /// <summary>
+    /// funcion CALCULARPOSICIONFICHA calcula la posicion media de todo el area de juego para posicionar las Cartas
+    /// funcion CHIPSIZE calcula el tamaño de cada carta
+    /// </summary>
 
     private Vector2 CalcularPosicionFicha()
     {
@@ -75,6 +146,14 @@ public class JuegoMemoria : MonoBehaviour
         return new Vector2(maxSizeX,maxSizeY);
     }
 
+
+
+
+    //TEMPORIZADOR
+    /// <summary>
+    /// funcion COUNTDOWN calcula el tiempo
+    /// funcion DISPLAYTIME enseña el tiempo en pantalla
+    /// </summary>
     private void CountDown()
     {
         
@@ -104,4 +183,24 @@ public class JuegoMemoria : MonoBehaviour
         Timer.text = string.Format("{0:00}:{1:00}",minutes,seconds);
     }
 
+
+
+
+
+
+    //shuffle
+    System.Random rnd = new System.Random();
+
+    public void Shuffle<T>(IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rnd.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
 }
