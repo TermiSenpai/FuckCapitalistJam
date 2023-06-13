62c831ec2d507a638099eca7c1821b0829826b8f
TermiSenpai <escarpaprieto@gmail.com>
2023-06-13 11:03:06 +0200

    Entrada al modo furia

--- Assets/Scripts/Player/PlayerStress.cs
+++ Assets/Scripts/Player/PlayerStress.cs
@@ -1,25 +1,37 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public static class PlayerStress
 {
     private static float stress = 0;
     public static event EventHandler ValueChanged;
 
+    public static bool canModify = true;
+
     public static float Stress
     {
         get { return stress; }
         set
         {
-            if (stress != value)
+            if (stress != value && canModify)
             {
+                value = checkValue(value);                
+
                 stress = value;
                 OnValueChanged(); // Llamamos al evento cuando el valor cambia
             }
         }
     }
+
+    private static float checkValue(float setValue)
+    {
+        if (setValue > 100f)
+            return 100f;
+        else if (setValue < 0f)
+             return 0f;
+
+        return setValue;
+    }
+
     private static void OnValueChanged()
     {
         ValueChanged?.Invoke(null, EventArgs.Empty);
